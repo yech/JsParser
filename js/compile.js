d7692aab0154824132b59cc817b6c0dcdc49f89c
ye chunhua <yechunhua@gmail.com>
2013-01-09 17:01:17 +0800

    Compile class

--- js/compile.js
+++ js/compile.js
@@ -7,7 +7,179 @@ Compile.prototype = {
     context:null,
 
     evaluate:function (context) {
-        this.context = context;
-        return true;
+        this.context = (context !== undefined) ? context : {};
+        return this.getExpression(this.asts);
+    },
+
+    getExpression:function (ast) {
+        var exp = ast.expression;
+        var ret;
+        if (ast.type === 'expression') {
+
+            switch (ast.operator) {
+                case '+':
+                    ret = this.getExpression(exp[0]) + this.getExpression(exp[1]);
+                    break;
+
+                case '-':
+                    ret = this.getExpression(exp[0]) - this.getExpression(exp[1]);
+                    break;
+
+                case '/':
+                    ret = this.getExpression(exp[0]) / this.getExpression(exp[1]);
+                    break;
+
+                case '%':
+                    ret = this.getExpression(exp[0]) % this.getExpression(exp[1]);
+                    break;
+
+                case '*':
+                    ret = this.getExpression(exp[0]) * this.getExpression(exp[1]);
+                    break;
+
+                case '||':
+                    ret = this.getExpression(exp[0]) || this.getExpression(exp[1]);
+                    break;
+
+                case '&&':
+                    ret = this.getExpression(exp[0]) && this.getExpression(exp[1]);
+                    break;
+
+                case '>':
+                    ret = this.getExpression(exp[0]) > this.getExpression(exp[1]);
+                    break;
+
+                case '<':
+                    ret = this.getExpression(exp[0]) < this.getExpression(exp[1]);
+                    break;
+
+                case '==':
+                    ret = this.getExpression(exp[0]) == this.getExpression(exp[1]);
+                    break;
+
+                case '>=':
+                    ret = this.getExpression(exp[0]) >= this.getExpression(exp[1]);
+                    break;
+
+                case '<=':
+                    ret = this.getExpression(exp[0]) <= this.getExpression(exp[1]);
+                    break;
+
+                case '!=':
+                    ret = this.getExpression(exp[0]) != this.getExpression(exp[1]);
+                    break;
+
+                case 'minus':
+                    ret = -this.getExpression(exp[0]);
+                    break;
+
+                case 'not':
+                    ret = !this.getExpression(exp[0]);
+                    break;
+
+                case 'parenthesis':
+                    ret = this.getExpression(exp[0]);
+                    break;
+
+                default:
+                    return undefined;
+            }
+
+            return ret;
+        } else {
+            return this.getLiteral(ast);
+        }
+    },
+
+    getLiteral:function (literal) {
+
+        var type = literal.type;
+        var ret = '';
+
+        if (type == 'string') {
+            ret = literal.value;
+        } else if (type == 'number') {
+            ret = Number(literal.value);
+        } else if (type == 'bool') {
+            if (literal.value === "null") {
+                ret = null;
+            } else if (literal.value === 'false') {
+                ret = false;
+            } else if (literal.value === 'true') {
+                ret = true;
+            }
+        } else {
+            return this.getReferences(literal);
+        }
+
+        return ret;
+    },
+
+    getReferences:function (ast) {
+        var context = this.context;
+        var ret = context[ast.id];
+        if (ast.path !== undefined && ret !== undefined) {
+            for (var i = 0; i < ast.path.length; i++) {
+                var property = ast.path[i];
+                ret = this.getAttributes(property, ret);
+                if (ret === undefined) break;
+            }
+        }
+        return ret;
+    },
+
+    getAttributes:function (property, baseRef) {
+        /**
+         * type可以是method, index, property
+         */
+        var type = property.type;
+        var ret;
+        var id = property.id;
+        if (type === 'method') {
+            ret = this.getPropMethod(property, baseRef);
+        } else if (type === 'property') {
+            ret = baseRef[id];
+        } else {
+            ret = this.getPropIndex(property, baseRef);
+        }
+        return ret;
+    },
+
+
+    getPropMethod:function (property, baseRef) {
+
+        var id = property.id;
+        var ret = baseRef[id];
+        var args = [];
+
+        if (property.args !== undefined) {
+            for (var i = 0; i < property.args.length; i++) {
+                args.push(this.getLiteral(property.args[i]));
+            }
+        }
+
+        if (ret && ret.call) {
+            ret = ret.apply(baseRef, args);
+        } else {
+            ret = undefined;
+        }
+
+
+        return ret;
+    },
+
+    getPropIndex:function (property, baseRef) {
+        var ast = property.id;
+        var key;
+        if (ast.type === 'references') {
+            key = this.getReferences(ast);
+        } else {
+            key = ast.value;
+        }
+
+        var ret;
+        ret = baseRef[key];
+
+        return ret;
     }
 };
